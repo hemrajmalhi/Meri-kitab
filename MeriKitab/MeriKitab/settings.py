76dbf566a0dd04a602893b15c5766e74bfbe1f18
Hemraj Malhi <hemrajmalhi1234@gmail.com>
2023-08-20 13:34:37 +0500

    all changes all saved

--- MeriKitab/MeriKitab/settings.py
+++ MeriKitab/MeriKitab/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# template
+templates=os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'MeriKitab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [templates],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +79,15 @@ WSGI_APPLICATION = 'MeriKitab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # mysql database;
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MeriKitab', # Change example - blog_data
+        'USER': 'root', # Change
+        'PASSWORD': 'root', # Change
+        'HOST': 'localhost',
+        # 'PORT': '80',
     }
 }
 
